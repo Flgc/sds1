ffe8427a4c2e5f0530ac1918f9b26fc0f097e7ab
Fabio Luis <ml@fabiotecnico1.biz>
2020-09-15 22:51:06 -0300

    POST records - Implemented

---
+++ backend/src/main/java/biz/fabiotecnico1/dspesquisa/services/RecordService.java
@@ -0,0 +1,41 @@
+package biz.fabiotecnico1.dspesquisa.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import biz.fabiotecnico1.dspesquisa.dto.RecordDTO;
+import biz.fabiotecnico1.dspesquisa.dto.RecordInsertDTO;
+import biz.fabiotecnico1.dspesquisa.entities.Game;
+import biz.fabiotecnico1.dspesquisa.entities.Record;
+import biz.fabiotecnico1.dspesquisa.repositories.GameRepository;
+import biz.fabiotecnico1.dspesquisa.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+	
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		
+		Record entity = new Record();
+		
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());;
+		entity.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = repository.save(entity);
+		return new RecordDTO(entity);
+	}
+	
+}
