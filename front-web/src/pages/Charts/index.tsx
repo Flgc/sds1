7e6062d9eb98b300d794f02ddfab0addbaacb08a
Fabio Luis <ml@fabiotecnico1.biz>
2020-09-20 18:47:59 -0300

    Configuring front-web for production

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -25,7 +25,7 @@ const initialPieData = {
   series: [],
 };
 
-const BASE_URL = 'http://192.168.0.19:8080';
+const BASE_URL = 'https://sds1-flgc.herokuapp.com';
 
 const Charts = () => {
   const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -6,7 +6,7 @@ import { formatDate } from './helpers';
 import Pagination from './Pagination';
 import Filters from '../../components/Filters';
 
-const BASE_URL = 'http://192.168.0.19:8080';
+const BASE_URL = 'https://sds1-flgc.herokuapp.com';
 
 const Records = () => {
   const [recordsResponse, setRecordResponse] = useState<RecordsResponse>();
